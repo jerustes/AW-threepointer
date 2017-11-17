9452997358e59abb51b88fa36d2f24849fb52611
jerustes <Jorge@wifi-90-100.uc3m.es>
2017-11-17 11:38:22 +0100

    initial commit

---
+++ AW-NBA/src/webapp/Status.java
@@ -0,0 +1,34 @@
+package webapp;
+public class Status {
+	private int round;		//Jornada
+	private int points;
+	private char phase;
+
+	public int getRound() {
+		return round;
+	}
+
+	public void setRound(int round) {
+		this.round = round;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	public char getPhase() {
+		return phase;
+	}
+
+	public void setPhase(char phase) {
+		this.phase = phase;
+	}
+
+	public String toString() {
+		return "[" +round+ "][" +points+ "][" +phase+ "]";
+	}
+}
