8256b0807cdbe896aa4100b9ea090afdd4948f6b
Jose Manuel <Jose Manuel@PC_Jose>
2017-11-21 12:01:35 +0100

    public Date getPrimaryDate() { again

--- AW-NBA/src/webapp/Week.java
+++ AW-NBA/src/webapp/Week.java
@@ -13,7 +13,7 @@ public class Week {
 	public void setId(int id) {
 		this.id = id;
 	}
-	public Date getStartDate() {
+	public Date getNewDate() {
 		return startDate;
 	}
 	public void setStartDate(Date startDate) {
