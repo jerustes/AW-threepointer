7f35f19b7ef32374415d863f29b70461acfaeff0
josemprb <33448640+josemprb@users.noreply.github.com>
2017-11-21 12:51:22 +0100

    Create Login.java

---
+++ AW-NBA/Login.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+public class Login extends HttpServlet {
+ 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        HttpSession session = request.getSession();
+        PrintWriter out = response.getWriter();
+        
+        String email = session.getParameter("email");
+        String pass = request.getParameter("contrasena");
+        
+        if(Validate.checkUser(email, pass))
+        {
+            RequestDispatcher rs = request.getRequestDispatcher("Welcome");
+            rs.forward(request, response);
+        }
+        else
+        {
+           out.println("Username or Password incorrect");
+           RequestDispatcher rs = request.getRequestDispatcher("index.html");
+           rs.include(request, response);
+        }
+    }  
+}
