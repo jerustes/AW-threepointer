a5d659101980e817a1accc94d26f4f6855c7cefd
Jose Manuel <Jose Manuel@PC_Jose>
2017-11-23 17:48:18 +0100

    Update Login.java

--- AW-NBA/src/webapp/Login.java
+++ AW-NBA/src/webapp/Login.java
@@ -2,16 +2,19 @@ package webapp;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
 @SuppressWarnings("serial")
-@WebServlet("/login")
 public class Login extends HttpServlet {
  
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -19,17 +22,27 @@ public class Login extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         HttpSession session = request.getSession();
         PrintWriter out = response.getWriter();
-        String email = (String) session.getAttribute("email");
-        String pass = (String) session.getAttribute("contrasena");
-                           
-        if(Validate.checkUser(email,pass,session))   {
-            out.println("Autenticación correcta.");
-            RequestDispatcher rd = request.getRequestDispatcher("/** VISTA PRINCIPAL **/");
-            rd.forward(request, response);
+        String email = (String) request.getAttribute("email");
+        String pass = (String) request.getAttribute("contraseña");
+        Query query = ((Session) session).createQuery("SELECT mail, contrasena, rol FROM Usuario "
+        		+ "WHERE mail=:correo AND contrasena=:pwd");
+        query.setString("correo",email);
+        query.setString("pwd",pass);
+        List<?> list = query.list();
+        String correo = (String) list.get(0);
+        String clave = (String) list.get(1);
+        String rol = (String) list.get(2);
+        if (pass==clave && email==correo) {
+        	if(rol=="jugador") {
+        		/*VISTA JUGADOR*/
+        	} else if(rol=="admin") {
+        		/*VISTA ADMIN*/
+        	} else {
+        		/*ERROR*/
+        	}
         } else {
-           out.println("Usuario o contraseña incorrectas.");
-           RequestDispatcher rd = request.getRequestDispatcher("index.html");
-           rd.forward(request, response);
+        	/*NO HAY REGISTRO CORRECTO*/
         }
+        
     }
 }
