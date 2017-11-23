1804c34659df6fb494a53fe62ea66a1ca669b098
jerustes <Jorge@wifi-76-164.uc3m.es>
2017-11-23 12:48:39 +0100

    re

--- AW-NBA/src/webapp/RegisterUser.java
+++ AW-NBA/src/webapp/RegisterUser.java
@@ -15,6 +15,7 @@ public class RegisterUser extends HttpServlet {
 		response.setContentType("text/html; charset = UTF-8");
 		PrintWriter out = response.getWriter();
 		
+		//CHANGE TO SESSION
 		String name = request.getParameter("email");
 		String email = request.getParameter("password");
 		
