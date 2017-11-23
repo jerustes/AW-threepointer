9e0e6e76ac46c351d905c8bef60098599efa3850
jerustes <Jorge@wifi-76-164.uc3m.es>
2017-11-23 12:47:23 +0100

    Welcome and register HTML page
    Welcome and Register Servlet (esqueleto only)

---
+++ AW-NBA/src/webapp/RegisterUser.java
@@ -0,0 +1,34 @@
+package webapp;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.*;
+import java.sql.*;
+
+public class RegisterUser extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException{
+		
+		response.setContentType("text/html; charset = UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		String name = request.getParameter("email");
+		String email = request.getParameter("password");
+		
+		try {
+			//Database connection
+			//prepared statement HQL
+			
+			//when checked todo ok
+				//out.println("Register complete");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
