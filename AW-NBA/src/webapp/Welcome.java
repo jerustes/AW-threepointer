bf0f4cba46d1ecae40f195cc3a5f0f32f1e89d19
jerustes <Jorge@wifi-76-164.uc3m.es>
2017-11-23 12:29:52 +0100

    updated index: simple utility created Welcome.java : once user is logged in (after validation)

---
+++ AW-NBA/src/webapp/Welcome.java
@@ -0,0 +1,19 @@
+package webapp;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.*;
+import java.sql.*;
+
+public class Welcome extends HttpServlet {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException{
+		response.setContentType("text/html; charset = UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("Welcome user!");
+	}
+}
