2eea1229d6770e52e1339229f08809358ed86de8
josemprb <33448640+josemprb@users.noreply.github.com>
2017-12-01 10:45:37 +0100

    Create ViewLeague.java

---
+++ AW-NBA/src/webapp/ViewLeague.java
@@ -0,0 +1,57 @@
+package webapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import webapp.Entities.League;
+import webapp.Entities.Lineup;
+import webapp.Entities.User;
+
+@WebServlet("/JoinLeague")
+public class JoinLeague extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 939335133858407229L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		HttpSession session = request.getSession();
+		PrintWriter out = response.getWriter();
+		Configuration configuration = new Configuration();
+		configuration.configure(this.getClass().getResource("/hibernate.cfg.xml"));
+		configuration.addAnnotatedClass(League.class);
+		configuration.addAnnotatedClass(Lineup.class);
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+		Session ses = sessionFactory.openSession();
+		User user = (User) session.getAttribute("user");
+		if (user == null) {
+			out.println("Usuario o contraseña incorrectas");
+			response.sendRedirect("login.jsp");
+		}
+		int id = Integer.parseInt(request.getParameter("id"));
+	  String hql = "from liga where id = :id";
+		Query query = ses.createQuery(hql);
+		query.setParameter("id",id);
+		List<League> leagues = (List<League>) query.list();
+		League league = leagues.get(0);
+		session.setAttribute("liga",league);
+    response.sendRedirect("LeagueHomeServlet?id="+id);
+	}
+}
